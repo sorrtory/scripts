6bb0d0ed25696263c56c187d868f165fe7ce80e8
sorrtory-desktop <62436169+sorrtory@users.noreply.github.com>
2025-08-26 21:03:30 +0300

    test raw ublock alert

--- ublock.js
+++ ublock.js
@@ -1,18 +1,4 @@
-/// ublock.js text/javascript
-(function () {
- alert("HELLO " + document.location.href);
-
-  
+twitch-videoad.js text/javascript
+(function() {
+    alert("HELLO " + document.location.href);
 })();
-
-// ==UserScript==
-// @name         bye-bye vk/feed
-// @namespace    http://tampermonkey.net/
-// @version      2025-08-26
-// @description  Reditect from vk/feed -> vk/im
-// @author       You
-// @match        https://vk.com/*
-// @run-at       document-start
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==

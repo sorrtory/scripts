8a6516c3673c076ff4f6be480350e77392037558
sorrtory-desktop <62436169+sorrtory@users.noreply.github.com>
2025-08-26 21:31:47 +0300

    fix(ublock.js): restore commented code and update console log message

--- ublock.js
+++ ublock.js
@@ -1,31 +1,31 @@
 ublock.js text/javascript
 (function () {
     "use strict";
-    console.log("Hello World from injected script! 2");
-    // function checkAndRedirect() {
-    //   console.log("Redirecting: vk/feed -> vk/im");
-    //   const p = location.pathname;
-    //   if (p === "/" || p.startsWith("/feed")) {
-    //     location.replace("https://vk.com/im");
-    //   }
-    // }
+    console.log("Hello World from injected script!");
+    function checkAndRedirect() {
+      console.log("Redirecting: vk/feed -> vk/im");
+      const p = location.pathname;
+      if (p === "/" || p.startsWith("/feed")) {
+        location.replace("https://vk.com/im");
+      }
+    }
 
-    // // Initial load
-    // checkAndRedirect();
+    // Initial load
+    checkAndRedirect();
 
-    // // Listen for back/forward navigation
-    // window.addEventListener("popstate", checkAndRedirect);
+    // Listen for back/forward navigation
+    window.addEventListener("popstate", checkAndRedirect);
 
-    // // Hook pushState & replaceState to detect in-page navigation
-    // const origPushState = history.pushState;
-    // history.pushState = function (...args) {
-    //   origPushState.apply(this, args);
-    //   checkAndRedirect();
-    // };
+    // Hook pushState & replaceState to detect in-page navigation
+    const origPushState = history.pushState;
+    history.pushState = function (...args) {
+      origPushState.apply(this, args);
+      checkAndRedirect();
+    };
 
-    // const origReplaceState = history.replaceState;
-    // history.replaceState = function (...args) {
-    //   origReplaceState.apply(this, args);
-    //   checkAndRedirect();
-    // };
+    const origReplaceState = history.replaceState;
+    history.replaceState = function (...args) {
+      origReplaceState.apply(this, args);
+      checkAndRedirect();
+    };
 })();

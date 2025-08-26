a5ef29a1a031e65d7d2447d09c5ae46af6ff3b89
sorrtory-desktop <62436169+sorrtory@users.noreply.github.com>
2025-08-26 21:01:32 +0300

    test raw ublock

--- ublock.js
+++ ublock.js
@@ -1,38 +1,8 @@
-ublock.js text/javascript
+/// ublock.js text/javascript
 (function () {
  alert("HELLO " + document.location.href);
 
-  document.addEventListener("DOMContentLoaded", function () {
-    alert("Loaded: " + document.location.href);
-    if ( /(^|\.)vk\.(com|ru)$/.test(document.location.hostname) === false ) { return; }
-    console.log("Redirecting: vk/feed -> vk/im");
-
-    function checkAndRedirect() {
-      const p = location.pathname;
-      if (p === "/" || p.startsWith("/feed")) {
-        location.replace("https://vk.com/im");
-      }
-    }
-
-    // Initial load
-    checkAndRedirect();
-
-    // Listen for back/forward navigation
-    window.addEventListener("popstate", checkAndRedirect);
-
-    // Hook pushState & replaceState to detect in-page navigation
-    const origPushState = history.pushState;
-    history.pushState = function (...args) {
-      origPushState.apply(this, args);
-      checkAndRedirect();
-    };
-
-    const origReplaceState = history.replaceState;
-    history.replaceState = function (...args) {
-      origReplaceState.apply(this, args);
-      checkAndRedirect();
-    };
-  });
+  
 })();
 
 // ==UserScript==

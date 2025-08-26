5ffc831cde3840e2db27ca3178ffc8c9222eb9d1
sorrtory-desktop <62436169+sorrtory@users.noreply.github.com>
2025-08-26 21:42:42 +0300

    fix(ublock.js): move for tampermonkey for now

--- ublock.js
+++ ublock.js
@@ -1,31 +1,40 @@
-ublock.js text/javascript
-(function () {
-    "use strict";
-    console.log("Hello World from injected script!");
+// ==UserScript==
+// @name         bye-bye vk/feed
+// @namespace    http://tampermonkey.net/
+// @version      2025-08-26
+// @description  Reditect from vk/feed -> vk/im
+// @author       You
+// @match        https://vk.com/*
+// @run-at       document-start
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+'use strict';
     function checkAndRedirect() {
-      console.log("Redirecting: vk/feed -> vk/im");
-      const p = location.pathname;
-      if (p === "/" || p.startsWith("/feed")) {
-        location.replace("https://vk.com/im");
-      }
+        const p = location.pathname;
+        if (p === '/' || p.startsWith('/feed')) {
+            location.replace('https://vk.com/im');
+        }
     }
 
     // Initial load
     checkAndRedirect();
 
     // Listen for back/forward navigation
-    window.addEventListener("popstate", checkAndRedirect);
+    window.addEventListener('popstate', checkAndRedirect);
 
     // Hook pushState & replaceState to detect in-page navigation
     const origPushState = history.pushState;
-    history.pushState = function (...args) {
-      origPushState.apply(this, args);
-      checkAndRedirect();
+    history.pushState = function(...args) {
+        origPushState.apply(this, args);
+        checkAndRedirect();
     };
 
     const origReplaceState = history.replaceState;
-    history.replaceState = function (...args) {
-      origReplaceState.apply(this, args);
-      checkAndRedirect();
+    history.replaceState = function(...args) {
+        origReplaceState.apply(this, args);
+        checkAndRedirect();
     };
 })();
